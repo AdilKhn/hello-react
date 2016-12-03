d75d7829cbc3ce9f04d4e4f706b25c32b1f234a3
me <me@here.com>
2016-12-03 18:56:52 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Item from './todos/Item';
+import './index.css';
+
+ReactDOM.render(
+  <div>
+  <App />
+  <Item />
+  </div>,
+  document.getElementById('root')
+);
---
+++ src/todos/Item.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+  render() {
+    return (
+      <div className="Item">
+        <p> Some Item </p>
+      </div>
+    );
+  }
+}
+export default Item;
