37e0e7bd398be0035080f6ec42dbbed25a36e581
Adil Khan <adil.khan@outlook.com>
2017-01-03 18:43:22 -0500

    [WIP] Ready to mount/unmount

--- src/index.js
+++ src/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function tick() {
-  const element = (
-    <div>
-      <h1> Hello, world </h1>
-      <h2> It is {new Date().toLocaleTimeString()} </h2>
-    </div>
-  );
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1> Hello, world </h1>
+//       <h2> It is {props.date.toLocaleTimeString()} </h2>
+//     </div>
+//   );
+// }
 
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date()};
+  }
+
+  componentDidMount() {
+  
+  }
+
+  componentWillMount() {
+  
+  }
+  render() {
+    return (
+      <div>
+        <h1> Hello, Woild </h1>
+        <h2> It is {this.state.date.toLocaleTimeString()}. </h2>
+      </div>
+    );
+  }
 }
 
+function tick() {
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );
+
+}
 setInterval(tick, 1000);
