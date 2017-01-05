0419a64baa091c3bbef8ecfb0b6dd205d5edefe5
Adil Khan <adil.khan@outlook.com>
2017-01-05 08:36:50 -0500

    Some Changes To Help/Illustrate understanding

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// function Clock(props) {
-//   return (
-//     <div>
-//       <h1> Hello, world </h1>
-//       <h2> It is {props.date.toLocaleTimeString()} </h2>
-//     </div>
-//   );
-// }
+function Banner(props) {
+  return (
+    <div>
+      <h1> This is a banner from a function </h1>
+      <h2> </h2>
+    </div>
+  );
+}
 
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date()};
+    this.state = { 
+      date: new Date(),
+      place: props.place || "Home"
+    };
   }
 
   componentDidMount() {
     this.timerID = setInterval(
-      ()=> this.tick(),
+      () => { this.tick();},
       1000
     );
   }
@@ -34,7 +37,7 @@ class Clock extends React.Component {
   render() {
     return (
       <div>
-        <h1> Hello, Woild </h1>
+        <h1> Hello, {this.state.place} </h1>
         <h2> It is {this.state.date.toLocaleTimeString()}. </h2>
       </div>
     );
@@ -42,7 +45,10 @@ class Clock extends React.Component {
 }
 
 ReactDOM.render(
-  <Clock/>,
+  <div>
+    <Banner/>
+    <Clock place="Jupiter"/>
+  </div>,
   document.getElementById('root')
 
 );
