1072e000dc5bd0a89e399c3155332f3426792640
Adil Khan <adil.khan@outlook.com>
2017-08-13 10:35:43 -0400

    Adds tests for state and props

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,17 @@ export default class Main extends Component{
 
   constructor(props){
     super(props);
-    this.state = {myname: 'homa'}
+    this.state = {foo: 'in constructor'}
+  }
+  sayHello(){
+      return 'Hello from Main' ;
   }
   render() {
     return(
       <div>
         <p>hi there</p>
+        <p>{this.state.foo}</p>
+        <p>{this.sayHello()}</p>
       </div>
     ) 
   }
--- src/components/Main.spec.js
+++ src/components/Main.spec.js
@@ -9,9 +9,20 @@ describe('<Main/>', () => {
   });
 
   it('has props', () => {
-  
-    const comp = shallow(<Main foo={'homy'}/>, {myname: 'hoomie'});
+    const comp = shallow(<Main foo={'in tag'}/>, {foo: 'in context'});
     console.log(comp.state());
     console.log(comp.instance().props);
   })
+
+  it('Execute the method on the component', ()=>{
+    const comp = shallow(<Main/>);
+    expect(comp.instance().sayHello()).to.equal('Hello from Main');
+  });
+
+  it('Set state from enzyme', () =>{
+      const comp = shallow(<Main/>); 
+      comp.setState({name: 'homer'});
+
+      expect(comp.state().name).to.equal('homer');
+  });
 });
