35e2e25df7475482540dea8ecbbaaee69ca4fab8
Adil Khan <adil.khan@outlook.com>
2017-08-13 12:09:07 -0400

    Tests for child

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Child from './Child.js';
 
 export default class Main extends Component{
 
@@ -9,15 +10,20 @@ export default class Main extends Component{
   sayHello(){
       return 'Hello from Main' ;
   }
+  sayChildHello(){
+    return 'Hello from Main Child!';
+  }
   render() {
     return(
       <div>
         <p>hi there</p>
         <p>{this.state.foo}</p>
         <p>{this.sayHello()}</p>
+        <p><Child doThis={this.sayChildHello}/></p>
       </div>
     ) 
   }
 
 
 }
+
--- src/components/Main.spec.js
+++ src/components/Main.spec.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
 import Main from './Main';
+import Child from './Child';
 describe('Tests for <Main/>', () => {
 
   it('loads without crashing', ()=>{
@@ -17,7 +18,19 @@ describe('Tests for <Main/>', () => {
   it('set state from enzyme', () =>{
     const comp = shallow(<Main/>); 
     comp.setState({name: 'homer'});
-
     expect(comp.state().name).to.equal('homer');
   });
+
 });
+
+
+describe('Tests Child', ()=>{
+  it('Test function that is passed from main to child', () => {
+    let dummyFunc = function() {
+        return 'Hi I am a dummy func';
+    }
+    const child = shallow(<Child doThis={dummyFunc}/>); 
+    expect(child.instance().props.doThis()).to.equal('Hi I am a dummy func');
+  });
+});
+
