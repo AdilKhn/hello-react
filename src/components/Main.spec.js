e422f2e0187aa888764e2065873f8339b839a2f1
Adil Khan <adil.khan@outlook.com>
2017-08-13 11:31:03 -0400

    Better reporting with plugin

--- src/components/Main.spec.js
+++ src/components/Main.spec.js
@@ -2,27 +2,22 @@ import React, {Component} from 'react';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
 import Main from './Main';
-describe('<Main/>', () => {
+describe('Tests for <Main/>', () => {
 
   it('loads without crashing', ()=>{
     expect(shallow(<Main/>).instance()).to.be.an.instanceOf(Main);
   });
 
-  it('has props', () => {
-    const comp = shallow(<Main foo={'in tag'}/>, {foo: 'in context'});
-    console.log(comp.state());
-    console.log(comp.instance().props);
-  })
 
   it('Execute the method on the component', ()=>{
     const comp = shallow(<Main/>);
     expect(comp.instance().sayHello()).to.equal('Hello from Main');
   });
 
-  it('Set state from enzyme', () =>{
-      const comp = shallow(<Main/>); 
-      comp.setState({name: 'homer'});
+  it('set state from enzyme', () =>{
+    const comp = shallow(<Main/>); 
+    comp.setState({name: 'homer'});
 
-      expect(comp.state().name).to.equal('homer');
+    expect(comp.state().name).to.equal('homer');
   });
 });
